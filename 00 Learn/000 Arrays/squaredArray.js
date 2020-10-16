35b6ffc97cba4e906763e8e11b3f64befae89ffd
Driijko <driijko@gmail.com>
2020-10-16 14:38:08 -0400

    First pass at squared array problem, Arrays module

--- 00 Learn/000 Arrays/squaredArray.js
+++ 00 Learn/000 Arrays/squaredArray.js
@@ -1,3 +1,57 @@
-function squaredArray(originalArray) {
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortedSquares = function(A) {
+
+    if (A.length < 1) {
+        return A;
+    }
     
-}
+    const squaredArray = [];
+    let searchIndex = 0;
+    let posNumIndex;
+    let negNumIndex;
+    
+    while (A[searchIndex] < 0 && searchIndex < (A.length - 1)) {
+        searchIndex++;
+    }
+
+    if (searchIndex === A.length - 1) {
+        posNumIndex = null;
+        negNumIndex = A.length - 1;
+    }
+    else if (A[0] >= 0) {
+        negNumIndex = null;
+        posNumIndex = 0;
+    }
+    else {
+        negNumIndex = searchIndex - 1;
+        posNumIndex = searchIndex;
+    }
+
+    for (let i = 0 ; i < A.length ; i++) {
+        if (
+            !A[negNumIndex]
+            || Math.abs(A[negNumIndex]) > A[posNumIndex]
+        ) {
+            squaredArray[i] = Math.pow(A[posNumIndex], 2);
+            posNumIndex++;
+        }
+        else if (
+            !A[posNumIndex]
+            || Math.abs(A[negNumIndex]) <= A[posNumIndex]
+        ) {
+            squaredArray[i] = Math.pow(A[negNumIndex], 2);
+            negNumIndex--;
+        }
+    }
+    
+    return squaredArray;
+};
+
+const array = [-5, -3, -2, -1];
+// const array = [0, 1, 2, 3] ;
+// const array = [-3, -1, 0, 2, 4];
+
+console.log(sortedSquares(array));
