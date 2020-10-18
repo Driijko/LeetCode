59f46d87768cda3897e8b3deb9d260f54dc81bbf
Driijko <driijko@gmail.com>
2020-10-18 11:15:31 -0400

    Second pass at remove element problem, Arrays module

--- 00 Learn/000 Arrays/removeNum.js
+++ 00 Learn/000 Arrays/removeNum.js
@@ -9,23 +9,30 @@ const removeElement = function(nums, val) {
     let lenRowOfVal = 1;
 
     while (currentIndex < nums.length) {
+
         if (nums[currentIndex] === val) {
-            while (nums[currentIndex + lenRowOfVal] === val && nums[currentIndex + lenRowOfVal] < nums.length) {
+
+            while (
+                nums[currentIndex + lenRowOfVal] === val 
+                && (currentIndex + lenRowOfVal) < nums.length
+            ) {
                 lenRowOfVal++;
             }
+
             nums.splice(currentIndex, lenRowOfVal);
+            lenRowOfVal = 1;
         }
+
         currentIndex++;
     }
 
     return nums.length;
-
 };
 
 // const nums = [0,1,2,2,3,0,4,2];
-const nums = [0];
+const nums = [4,2,0,2,2,1,4,4,1,4,3,2];
 
-const val = 0;
+const val = 4;
 
 console.log(removeElement(nums, val));
 console.log(nums);
