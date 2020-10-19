9318bf565c85d1543da18204b8ce1500c4a2f794
Driijko <driijko@gmail.com>
2020-10-19 13:24:13 -0400

    Third pass at move zeros problem, Arrays module

--- 00 Learn/000 Arrays/moveZeros.js
+++ 00 Learn/000 Arrays/moveZeros.js
@@ -8,30 +8,23 @@ var moveZeroes = function(nums) {
         return;
     }
     
-    let numOfFrontZeros = 0;
+    // let numOfZeros = 0;
     let writePointer = 0;
 
     nums.forEach((num, index)=> {
 
-        if (
-            num === 0
-            && index < (nums.length - numOfFrontZeros)
-        ) {
-            numOfFrontZeros++;
-        }
-        else {
+        if (num !== 0) {
             nums[writePointer] = nums[index];
             writePointer++;
         }
-
-        if (index >= (nums.length - numOfFrontZeros)) {
-            nums[index] = 0;
-        }
-
     })
+
+    for (let i = writePointer ; i < nums.length ; i++) {
+        nums[i] = 0;
+    }
 };
 
-const array = [0,1, 0, 1];
+const array = [0,0,1, 0, 1, 43, 62, 12, 0, 19];
 moveZeroes(array);
 console.log(array);
 // [1,3,12,0,0]
